9ebdcf1d76f90634d76899e0fde4ac87b90a72df
Chan01116 <ktiun9630@gmail.com>
2025-03-06 16:15:05 +0900

    구구단2

--- src/javaStudy/Study.java
+++ src/javaStudy/Study.java
@@ -3,35 +3,34 @@ package javaStudy;
 public class Study {
 
 	public static void main(String[] args) {
-		
 		// 문제 : 아래와 같이 출력해주세요.
-		int i = 8;
+		// 문제 : dan 변수의 값에 따라 그에 해당하는 구구단이 출력 되도록 해주세요.
 		
-		System.out.println(i*1);
-		System.out.println(i*2);
-		System.out.println(i*3);
-		System.out.println(i*4);
-		System.out.println(i*5);
-		System.out.println(i*6);
-		System.out.println(i*7);
-		System.out.println(i*8);
-		System.out.println(i*9);
+		 int dan = 8;
 		
-		
-		
-		
-		// 출력
-	    /*
-	    8 * 1 = 8
-	    8 * 2 = 16
-	    8 * 3 = 24
-	    8 * 4 = 32
-	    8 * 5 = 40
-	    8 * 6 = 48
-	    8 * 7 = 56
-	    8 * 8 = 64
-	    8 * 9 = 72
-	    */
+		  System.out.println(dan + " * 1 = 8");
+		    System.out.println(dan + " * 2 = 16");
+		    System.out.println(dan + " * 3 = 24");
+		    System.out.println(dan + " * 4 = 32");
+		    System.out.println(dan + " * 5 = 40");
+		    System.out.println(dan + " * 6 = 48");
+		    System.out.println(dan + " * 7 = 56");
+		    System.out.println(dan + " * 8 = 64");
+		    System.out.println(dan + " * 9 = 72");
+		    
+		    // 출력
+		    /*
+		    8 * 1 = 8
+		    8 * 2 = 16
+		    8 * 3 = 24
+		    8 * 4 = 32
+		    8 * 5 = 40
+		    8 * 6 = 48
+		    8 * 7 = 56
+		    8 * 8 = 64
+		    8 * 9 = 72
+		    */
+	
 		
 	}
 }
