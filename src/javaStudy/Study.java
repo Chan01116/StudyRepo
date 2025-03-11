e103960b28dc7c7ed4c77963c5691ea5a62b4948
Chan01116 <ktiun9630@gmail.com>
2025-03-11 14:46:17 +0900

    반복문을 써서 1000까지 구구단

--- src/javaStudy/Study.java
+++ src/javaStudy/Study.java
@@ -5,31 +5,19 @@ public class Study {
 	public static void main(String[] args) {
 		// 문제 : 아래와 같이 출력해주세요.
 		// 문제 : 2부터 9까지의 숫자를 사용하지 않고 구구단 출력을 완성 해주세요.
-		// 조건 : * 1000까지 출력해주세요 (for, while x)
+		// 조건 : 반복문을 써서 하기
 		int dan = 8;
 	    
 	    // 수정가능지역 시작
 	    int i = 1;
 	    
 	    
-	    if(i <= 1000) {
+	    while(i <= 1000) {
 	    	System.out.println(dan + " * " + (i) + " = " + dan * i);
 		    i++;
 	    }
-	    if(i <= 1000) {
-	    	System.out.println(dan + " * " + (i) + " = " + dan * i);
-		    i++;
-	    }if(i <= 1000) {
-	    	System.out.println(dan + " * " + (i) + " = " + dan * i);
-		    i++;
-	    }if(i <= 1000) {
-	    	System.out.println(dan + " * " + (i) + " = " + dan * i);
-		    i++;
-	    }if(i <= 1000) {
-	    	System.out.println(dan + " * " + (i) + " = " + dan * i);
-		    i++;
-	    }
-	    // 이런식으로 1000개 이상 쓰면 천까지만 나온다.
+	  
+	    // 반복문은 if대신 while을 쓰면 된다.
 	    
 	    // 수정가능지역 끝
 	    
