a145f4e2eb59d276a34a6eee4def8638f59b1ec1
Chan01116 <ktiun9630@gmail.com>
2025-03-11 14:44:14 +0900

    반복문없이 구구단 1000까지

--- src/javaStudy/Study.java
+++ src/javaStudy/Study.java
@@ -4,32 +4,47 @@ public class Study {
 
 	public static void main(String[] args) {
 		// 문제 : 아래와 같이 출력해주세요.
-		// 문제 : dan 변수의 값에 따라 그에 해당하는 구구단이 출력 되도록 해주세요.
-		
-		 int dan = 8;
-		
-		  System.out.println(dan + " * 1 = 8");
-		    System.out.println(dan + " * 2 = 16");
-		    System.out.println(dan + " * 3 = 24");
-		    System.out.println(dan + " * 4 = 32");
-		    System.out.println(dan + " * 5 = 40");
-		    System.out.println(dan + " * 6 = 48");
-		    System.out.println(dan + " * 7 = 56");
-		    System.out.println(dan + " * 8 = 64");
-		    System.out.println(dan + " * 9 = 72");
-		    
-		    // 출력
-		    /*
-		    8 * 1 = 8
-		    8 * 2 = 16
-		    8 * 3 = 24
-		    8 * 4 = 32
-		    8 * 5 = 40
-		    8 * 6 = 48
-		    8 * 7 = 56
-		    8 * 8 = 64
-		    8 * 9 = 72
-		    */
+		// 문제 : 2부터 9까지의 숫자를 사용하지 않고 구구단 출력을 완성 해주세요.
+		// 조건 : * 1000까지 출력해주세요 (for, while x)
+		int dan = 8;
+	    
+	    // 수정가능지역 시작
+	    int i = 1;
+	    
+	    
+	    if(i <= 1000) {
+	    	System.out.println(dan + " * " + (i) + " = " + dan * i);
+		    i++;
+	    }
+	    if(i <= 1000) {
+	    	System.out.println(dan + " * " + (i) + " = " + dan * i);
+		    i++;
+	    }if(i <= 1000) {
+	    	System.out.println(dan + " * " + (i) + " = " + dan * i);
+		    i++;
+	    }if(i <= 1000) {
+	    	System.out.println(dan + " * " + (i) + " = " + dan * i);
+		    i++;
+	    }if(i <= 1000) {
+	    	System.out.println(dan + " * " + (i) + " = " + dan * i);
+		    i++;
+	    }
+	    // 이런식으로 1000개 이상 쓰면 천까지만 나온다.
+	    
+	    // 수정가능지역 끝
+	    
+	    // 출력
+	    /*
+	    8 * 1 = 8
+	    8 * 2 = 16
+	    8 * 3 = 24
+	    8 * 4 = 32
+	    8 * 5 = 40
+	    8 * 6 = 48
+	    8 * 7 = 56
+	    8 * 8 = 64
+	    8 * 9 = 72
+	    */
 	
 		
 	}
